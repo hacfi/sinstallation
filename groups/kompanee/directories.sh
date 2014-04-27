2c951ab27ad566fe3ffca330041890423d800709
Jeff Felchner <accounts+git@thekompanee.com>
2014-04-26 22:18:14 -0500

    Also make the ~/Downloads/skip-backup directory

--- groups/kompanee/directories.sh
+++ groups/kompanee/directories.sh
@@ -2,4 +2,5 @@ function create_directory_structure {
   mkdir -p $HOME/Kompanee 2> /dev/null
   mkdir -p $HOME/Boxes 2> /dev/null
   mkdir -p $HOME/Projects 2> /dev/null
+  mkdir -p $HOME/Downloads/skip-backup 2> /dev/null
 }
