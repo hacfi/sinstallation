7a7d1ab862d18e6a3335511eab9520958b447f07
Jeff Felchner <accounts+git@thekompanee.com>
2014-04-27 00:44:06 -0500

    Completely unloading spotlight causes problems in certain situations so give users the option of disabling it and/or unloading it

--- preferences/spotlight.sh
+++ preferences/spotlight.sh
@@ -7,22 +7,26 @@ function osx_spotlight {
   local loaded;  if [[ "$1" == "enabled" ]]; then loaded="load"; else loaded="unload"; fi
   shift 1;
 
-  if [[ "$enabled" == "off" ]]; then
+  if [[ "$enabled" == "off" || "$enabled" == "unload" ]]; then
     sudo sh -c "mdutil -a -E -i $enabled &> /dev/null"
     sudo sh -c "mdutil -a -d -E &> /dev/null"
     sudo sh -c "rm -rf /.Spotlight-V100 2> /dev/null"
+    sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
+  else
+    sudo mdutil -a -i $enabled
+    sudo mdutil -a -E
+    sudo chmod 777 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
+  fi
+
+  if [[ "$enabled" == "unload" ]]; then
     sudo sh -c "launchctl $loaded -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist 2> /dev/null"
     sudo sh -c "launchctl $loaded -w /System/Library/LaunchDaemons/com.apple.metadata.mds.index.plist 2> /dev/null"
     sudo sh -c "launchctl $loaded -w /System/Library/LaunchDaemons/com.apple.metadata.mds.scan.plist 2> /dev/null"
     sudo sh -c "launchctl $loaded -w /System/Library/LaunchDaemons/com.apple.metadata.mds.spindump.plist 2> /dev/null"
-    sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
   else
     sudo launchctl $loaded -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
     sudo launchctl $loaded -w /System/Library/LaunchDaemons/com.apple.metadata.mds.index.plist
     sudo launchctl $loaded -w /System/Library/LaunchDaemons/com.apple.metadata.mds.scan.plist
     sudo launchctl $loaded -w /System/Library/LaunchDaemons/com.apple.metadata.mds.spindump.plist
-    sudo mdutil -a -i $enabled
-    sudo mdutil -a -E
-    sudo chmod 777 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
   fi
 }
