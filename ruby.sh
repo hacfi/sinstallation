2aaa8bac39f6f52efeb041073fdd80492f2aae01
Jeff Felchner <accounts+git@thekompanee.com>
2014-04-28 03:46:04 -0500

    Add a short term fix to get Ruby 2.1.1 to install on Mavericks with readline 6.3

--- ruby.sh
+++ ruby.sh
@@ -47,7 +47,8 @@ function install_ruby {
   echo 'redcarpet'   >> $RBENV_ROOT/default_gems
 
   if ! [ -d "/usr/local/var/rbenv/versions/2.1.1" ]; then
-    rbenv install 2.1.1
+    # Short-term fix for readline 6.3 so that Ruby 2.1.1 will install
+    curl -fsSL https://gist.github.com/mislav/a18b9d7f0dc5b9efc162.txt | rbenv install --patch 2.1.1
     rbenv global 2.1.1
   fi
 }
