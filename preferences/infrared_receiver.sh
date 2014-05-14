ce48b8db4058ad5fb09512d32a973c36172feae0
Jeff Felchner <accounts+git@thekompanee.com>
2014-05-14 00:15:08 -0500

    Bugfix: Stop removing the IR Receiver files
    Removing the IR Receiver extension was causing problems so we need to stop doing
    that.
    
    --------------------------------------------------------------------------------
    Change-Id:     Id71f9e76897bfafdc5915a05938d554c3f965136
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>

--- preferences/infrared_receiver.sh
+++ preferences/infrared_receiver.sh
@@ -7,8 +7,4 @@ function osx_ir_receiver {
   local enabled; if [[ "$1" == "enabled" ]]; then enabled="true"; else enabled="false"; fi; shift 1;
 
   sudo defaults write /Library/Preferences/com.apple.driver.AppleIRController DeviceEnabled -bool $enabled
-
-  if [[ "$enabled" == "false" ]]; then
-    sudo rm -rf /System/Library/Extensions/AppleIRController.kext
-  fi
 }
