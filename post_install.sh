9e6322cee352fe194f83dcfd9d937d85709fd0b7
Jeff Felchner <accounts+git@thekompanee.com>
2014-05-24 18:50:27 -0500

    Bugfix: Post-Install not deleting image files

--- post_install.sh
+++ post_install.sh
@@ -1,32 +1,35 @@
-function post_install {
-  rm -rf "$HOME/Downloads/*.dmg"
-  rm -rf "$HOME/Downloads/*.zip"
-  rm -rf "$HOME/Downloads/*.pkg"
-  rm -rf "$HOME/Downloads/*.tbz"
-  rm -rf "$HOME/Downloads/*.cdr"
+rm -rf $HOME/Downloads/*.dmg
+rm -rf $HOME/Downloads/*.zip
+rm -rf $HOME/Downloads/*.pkg
+rm -rf $HOME/Downloads/*.tbz
+rm -rf $HOME/Downloads/*.cdr
+rm -rf $HOME/.subversion
+rm -rf $HOME/.gem
+rm -rf $HOME/.cache
+rm -rf $HOME/.pip
+rm -rf $HOME/.distlib
 
-  echo ""
-  echo "Manual Steps:"
-  echo "  * Set your profile photo"
-  echo "  * Switch Caps Lock to Control"
-  echo "  * Setup Google Accounts in System Preferences"
-  echo "      * Setup Calendar"
-  echo "      * Setup Contacts"
-  echo "  * Set your firmware password:"
-  echo "      * Restart your computer"
-  echo "      * Hold down Option"
-  echo "      * Boot to the Recovery Disk"
-  echo "      * Click Tools in the menu bar"
-  echo "      * Open Firmware Utility"
-  echo "      * Set a passphrase that is typable but difficult to guess"
-  echo "      * Put the passphrase in 1Password"
-  echo "  * After restarting with your profile photo set, enable FileVault"
-  echo "      * Open Terminal"
-  echo "      * Type this: sudo fdesetup enable"
-  echo "      * Keep your FileVault key in 1Password"
-  echo "      * Don't shut down your computer till FileVault completes"
-  echo ""
+echo ""
+echo "Manual Steps:"
+echo "  * Set your profile photo"
+echo "  * Switch Caps Lock to Control"
+echo "  * Setup Google Accounts in System Preferences"
+echo "      * Setup Calendar"
+echo "      * Setup Contacts"
+echo "  * Set your firmware password:"
+echo "      * Restart your computer"
+echo "      * Hold down Option"
+echo "      * Boot to the Recovery Disk"
+echo "      * Click Tools in the menu bar"
+echo "      * Open Firmware Utility"
+echo "      * Set a passphrase that is typable but difficult to guess"
+echo "      * Put the passphrase in 1Password"
+echo "  * After restarting with your profile photo set, enable FileVault"
+echo "      * Open Terminal"
+echo "      * Type this: sudo fdesetup enable"
+echo "      * Keep your FileVault key in 1Password"
+echo "      * Don't shut down your computer till FileVault completes"
+echo ""
 
-  cd "$HOME"
-  rm -rf "$HOME/sinstallation"
-}
+cd "$HOME"
+rm -rf "$HOME/sinstallation"
