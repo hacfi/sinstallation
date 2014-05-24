5d39a0fad7df81c09e62e1d800a6f7dfcabfc679
Jeff Felchner <accounts+git@thekompanee.com>
2014-05-23 23:12:26 -0500

    Feature: Add a profile for hestia

---
+++ profiles/hestia.sh
@@ -0,0 +1,10 @@
+source './profiles/hestia/apps.sh'
+source './profiles/hestia/preferences.sh'
+source './profiles/hestia/login_items.sh'
+source './profiles/hestia/cleanup.sh'
+
+install_apps
+set_preferences
+add_login_items
+
+cleanup
