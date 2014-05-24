6b790cadfdff1cb794bffebf5186fcdb21c9e8e9
Jeff Felchner <accounts+git@thekompanee.com>
2014-05-24 14:48:27 -0500

    Docs: Remove KeyRemap4Macbook from roadmap

--- profiles/jfelchner.sh
+++ profiles/jfelchner.sh
@@ -7,6 +7,4 @@ install_apps
 set_preferences
 add_login_items
 
-wget -O "$HOME/Library/Application Support/KeyRemap4MacBook/private.xml" "https://gist.githubusercontent.com/msolomon/9614362/raw/3b156d8cd9c006941f28a77fc193b29599377ab9/private.xml"
-
 cleanup
