fa8b1cc724d8d12e7a870c65e0888586048f58fb
Jeff Felchner <accounts+git@thekompanee.com>
2014-06-09 00:11:23 -0500

    Feature: Add a profile for blakecash

---
+++ profiles/blakecash.sh
@@ -0,0 +1,9 @@
+source './profiles/blakecash/apps.sh'
+source './profiles/blakecash/preferences.sh'
+source './profiles/blakecash/login_items.sh'
+source './profiles/blakecash/cleanup.sh'
+
+install_apps
+set_preferences
+add_login_items
+cleanup
