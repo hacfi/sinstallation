4a503da705fea919b74118cb2714a7456f64dce7
Jeff Felchner <accounts+git@thekompanee.com>
2014-11-28 14:11:09 -0600

    Bugfix: Don’t reset the desktop wallpaper when resetting Launchpad
    https://github.com/mathiasbynens/dotfiles/commit/db46bccca91947b5a2d557623160ac71bca53a7c
    
    --------------------------------------------------------------------------------
    Change-Id:     I3946c764248cb2da9bd0d8f45f5b74c753801f24
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>

--- preferences/launchpad.sh
+++ preferences/launchpad.sh
@@ -4,5 +4,5 @@
 
 # Reset LaunchPad
 function osx_launchpad_reset {
-  find "/Users/$(whoami)/Library/Application Support/Dock" -name '*.db' -maxdepth 1 -delete
+  find "/Users/$(whoami)/Library/Application Support/Dock" -name "*-*.db" -maxdepth 1 -delete
 }
