1ef602911743b8c799e84edaab243923388e3ff4
Jeff Felchner <accounts+git@thekompanee.com>
2014-11-28 14:40:07 -0600

    Feature: Add 'Applications' folder to the user's directory
    This technically happens automatically when `brew cask` is used, but I wanted to
    be explicit.
    
    --------------------------------------------------------------------------------
    Change-Id:     I5cc699d6d75fc7c1f4eebfae290a9d80f1df8d3f
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>

--- profiles/kompanee/directories.sh
+++ profiles/kompanee/directories.sh
@@ -1,4 +1,5 @@
 function create_directory_structure {
+  mkdir -p $HOME/Applications 2> /dev/null
   mkdir -p $HOME/Kompanee 2> /dev/null
   mkdir -p $HOME/Boxes 2> /dev/null
   mkdir -p $HOME/Projects 2> /dev/null
