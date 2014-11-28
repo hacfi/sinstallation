8fd6a20322492dfecfb23971258e5409e6a6cff4
Jeff Felchner <accounts+git@thekompanee.com>
2014-11-28 14:41:32 -0600

    Feature: Setup preference for Spotlight for proper indexing
    --------------------------------------------------------------------------------
    Change-Id:     I0e0744bf9596c81469fc6f2f1628978a14343ff6
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>

--- preferences/spotlight.sh
+++ preferences/spotlight.sh
@@ -7,6 +7,31 @@ function osx_spotlight {
   local loaded;  if [[ "$1" == "enabled" ]]; then loaded="load"; else loaded="unload"; fi
   shift 1;
 
+  # Change indexing order and disable some search results
+  defaults write com.apple.spotlight orderedItems -array \
+    '{"enabled" = 0;"name" = "APPLICATIONS";}' \
+    '{"enabled" = 0;"name" = "SYSTEM_PREFS";}' \
+    '{"enabled" = 0;"name" = "DIRECTORIES";}' \
+    '{"enabled" = 0;"name" = "PDF";}' \
+    '{"enabled" = 0;"name" = "FONTS";}' \
+    '{"enabled" = 0;"name" = "DOCUMENTS";}' \
+    '{"enabled" = 0;"name" = "MESSAGES";}' \
+    '{"enabled" = 0;"name" = "CONTACT";}' \
+    '{"enabled" = 0;"name" = "EVENT_TODO";}' \
+    '{"enabled" = 0;"name" = "IMAGES";}' \
+    '{"enabled" = 0;"name" = "BOOKMARKS";}' \
+    '{"enabled" = 0;"name" = "MUSIC";}' \
+    '{"enabled" = 0;"name" = "MOVIES";}' \
+    '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
+    '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+    '{"enabled" = 0;"name" = "SOURCE";}' \
+    '{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
+    '{"enabled" = 0;"name" = "MENU_OTHER";}' \
+    '{"enabled" = 0;"name" = "MENU_CONVERSION";}' \
+    '{"enabled" = 0;"name" = "MENU_EXPRESSION";}' \
+    '{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
+    '{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
+
   if [[ "$enabled" == "off" || "$enabled" == "unload" ]]; then
     sudo sh -c "mdutil -a -E -i $enabled &> /dev/null"
     sudo sh -c "mdutil -a -d -E &> /dev/null"
