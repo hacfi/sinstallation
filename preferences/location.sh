1caa84a88bdd03b92373f3eca1995bd51c356824
Jeff Felchner <accounts+git@thekompanee.com>
2014-11-29 16:37:52 -0600

    Feature: Update location services preference for Yosemite

--- preferences/location.sh
+++ preferences/location.sh
@@ -3,9 +3,12 @@
 ################################################################################
 
 function osx_location {
-  local enabled; if [[ "$1" == "enabled" ]]; then enabled="false"; else enabled="true"; fi
   local loaded; if [[ "$1" == "enabled" ]]; then loaded="load"; else loaded="unload"; fi
+  local enabled;     if [[ "$1" == "enabled" ]]; then enabled="true";  else enabled="false"; fi
+  local enabled_neg; if [[ "$1" == "enabled" ]]; then enabled="false"; else enabled="true"; fi
 
-  defaults write com.apple.MCX DisableLocationServices -bool $enabled
+  defaults write com.apple.MCX DisableLocationServices -bool $enabled_neg
+  defaults -currentHost write com.apple.locationd LocationServicesEnabled -bool $enabled
+
   sudo sh -c "launchctl $loaded -w /System/Library/LaunchDaemons/com.apple.locationd.plist 2> /dev/null"
 }
