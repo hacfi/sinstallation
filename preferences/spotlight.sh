0020c893613ae89a8d1a894966ff5028128fcbc7
Jeff Felchner <accounts+git@thekompanee.com>
2014-11-29 21:34:55 -0600

    Bugfix: Try setting the spotlight search types after enabling/disabling
    --------------------------------------------------------------------------------
    Change-Id:     Ia7d7615b6c264d7778f9491093f199790001287a
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>

--- preferences/spotlight.sh
+++ preferences/spotlight.sh
@@ -6,34 +6,10 @@ source './keyboard_shortcuts.sh'
 
 function osx_spotlight {
   local enabled;      if [[ "$1" == "enabled" ]]; then enabled="on";        else enabled="off";   fi
+  local enabled_int;  if [[ "$1" == "enabled" ]]; then enabled_int="1";     else enabled_int="0";   fi
   local enabled_bool; if [[ "$1" == "enabled" ]]; then enabled_bool="true"; else enabled_bool="false"; fi
   local loaded;       if [[ "$1" == "enabled" ]]; then loaded="load";       else loaded="unload"; fi
   shift 1;
-
-  # Change indexing order and disable some search results
-  defaults write com.apple.Spotlight orderedItems -array \
-    '{"enabled" = 0;"name" = "APPLICATIONS";}' \
-    '{"enabled" = 0;"name" = "SYSTEM_PREFS";}' \
-    '{"enabled" = 0;"name" = "DIRECTORIES";}' \
-    '{"enabled" = 0;"name" = "PDF";}' \
-    '{"enabled" = 0;"name" = "FONTS";}' \
-    '{"enabled" = 0;"name" = "DOCUMENTS";}' \
-    '{"enabled" = 0;"name" = "MESSAGES";}' \
-    '{"enabled" = 0;"name" = "CONTACT";}' \
-    '{"enabled" = 0;"name" = "EVENT_TODO";}' \
-    '{"enabled" = 0;"name" = "IMAGES";}' \
-    '{"enabled" = 0;"name" = "BOOKMARKS";}' \
-    '{"enabled" = 0;"name" = "MUSIC";}' \
-    '{"enabled" = 0;"name" = "MOVIES";}' \
-    '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
-    '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
-    '{"enabled" = 0;"name" = "SOURCE";}' \
-    '{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
-    '{"enabled" = 0;"name" = "MENU_OTHER";}' \
-    '{"enabled" = 0;"name" = "MENU_CONVERSION";}' \
-    '{"enabled" = 0;"name" = "MENU_EXPRESSION";}' \
-    '{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
-    '{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
 
   sudo killall mds > /dev/null 2>&1
 
@@ -49,6 +25,31 @@ function osx_spotlight {
     sudo mdutil -a -E
     sudo chmod 777 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
   fi
+
+  # Change indexing order and disable some search results
+  defaults write com.apple.Spotlight orderedItems -array \
+    "{\"enabled\" = $enabled_int; \"name\" = \"APPLICATIONS\";}" \
+    "{\"enabled\" = $enabled_int; \"name\" = \"SYSTEM_PREFS\";}" \
+    "{\"enabled\" = $enabled_int; \"name\" = \"DIRECTORIES\";}" \
+    "{\"enabled\" = $enabled_int; \"name\" = \"PDF\";}" \
+    "{\"enabled\" = $enabled_int; \"name\" = \"FONTS\";}" \
+    "{\"enabled\" = $enabled_int; \"name\" = \"DOCUMENTS\";}" \
+    "{\"enabled\" = $enabled_int; \"name\" = \"MESSAGES\";}" \
+    "{\"enabled\" = $enabled_int; \"name\" = \"CONTACT\";}" \
+    "{\"enabled\" = 0;            \"name\" = \"EVENT_TODO\";}" \
+    "{\"enabled\" = 0;            \"name\" = \"IMAGES\";}" \
+    "{\"enabled\" = 0;            \"name\" = \"BOOKMARKS\";}" \
+    "{\"enabled\" = 0;            \"name\" = \"MUSIC\";}" \
+    "{\"enabled\" = 0;            \"name\" = \"MOVIES\";}" \
+    "{\"enabled\" = 0;            \"name\" = \"PRESENTATIONS\";}" \
+    "{\"enabled\" = 0;            \"name\" = \"SPREADSHEETS\";}" \
+    "{\"enabled\" = 0;            \"name\" = \"SOURCE\";}" \
+    "{\"enabled\" = 0;            \"name\" = \"MENU_DEFINITION\";}" \
+    "{\"enabled\" = 0;            \"name\" = \"MENU_OTHER\";}" \
+    "{\"enabled\" = 0;            \"name\" = \"MENU_CONVERSION\";}" \
+    "{\"enabled\" = 0;            \"name\" = \"MENU_EXPRESSION\";}" \
+    "{\"enabled\" = 0;            \"name\" = \"MENU_WEBSEARCH\";}" \
+    "{\"enabled\" = 0;            \"name\" = \"MENU_SPOTLIGHT_SUGGESTIONS\";}"
 
   local spotlight_keyboard_shortcut_ids=(64 65)
 
